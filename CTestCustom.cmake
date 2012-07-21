ede2ae3ec57e4f5195e825fe3536114fa67585a1
hjmjohnson <hjmjohnson@fd2da0a4-d968-48df-9cb5-d8c496287cbf>
2012-07-21 16:26:08 +0000

    COMP:  Improve coverage information.
    Coverage should not include third party tools
    that we have no intention of testing.
    
    git-svn-id: https://advants.svn.sourceforge.net/svnroot/advants/trunk@1442 fd2da0a4-d968-48df-9cb5-d8c496287cbf

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -30,11 +30,8 @@
 #-- #Reset maximum number of warnings so that they all show up.
 set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 1000)
 
-#set(CTEST_CUSTOM_COVERAGE_EXCLUDE $ {CTEST_CUSTOM_COVERAGE_EXCLUDE}
-#    "./SlicerExecutionModel/"
-#    "./SlicerExecutionModel/.*"
-#    "./SlicerExecutionModel/.*/.*"
-#    ".*SlicerExecutionModel.*"
-#    "SlicerExecutionModel"
-#    )
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE ${CTEST_CUSTOM_COVERAGE_EXCLUDE}
+  ".*Temporary/.*"
+  ".*boost.*"
+)
 
